caa8e0ae69f15cbc41146cd98ffedfd9f93b6a10
aarekr <aare212@hotmail.com>
2022-10-19 10:56:17 +0300

    Tehtävä 3.5 step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       name: 'Arto Hellas',
@@ -50,6 +52,22 @@ app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
   res.status(204).end()
+})
+
+const generateId = () => {
+  const newId = Math.floor(1000 * Math.random())
+  return newId
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 const PORT = 3001
