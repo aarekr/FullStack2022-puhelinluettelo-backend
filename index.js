0e974ea9a956eeb7525a565e0baccfeb8aab63fa
aarekr <aare212@hotmail.com>
2022-10-20 09:59:12 +0300

    Tehtävä 3.13 step1

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('build'))
-
-let persons = [
-    {
-      name: 'Arto Hellas',
-      number: '040-1231244',
-      id: 1
-    },
-    {
-      name: 'Ada Lovelace',
-      number: '+31-231-12314',
-      id: 2
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345',
-      id: 3
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122',
-      id: 4
-    }
-]
 
 app.get('/', (req, res) => {
     res.send('<h1>Puhelinluettelo</h1>')
@@ -40,7 +19,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  Person.find({}).then(mongo_people => {
+    res.json(mongo_people)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -101,7 +82,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
